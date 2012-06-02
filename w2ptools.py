01bc18eb136c4812c493b283c286df26016ebafc
Ian W. Scott <iscott@tyndale.ca>
2012-06-01 22:33:33 -0400

    Significant reworking. PluginSync is now functional and allows for excluding files.

--- w2ptools.py
+++ w2ptools.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 
-import sys
 import re
 import os
 import glob
@@ -22,8 +21,9 @@ installation folder.
 class PluginSync(object):
     """Synchronize plugins within a single web2py directory"""
 
-    def __init__(self):
-        pass
+    def __init__(self, verbose=False):
+        self.verbose = verbose
+        self.skiplist = {'plugin_framework':['static/plugin_framework/theme.less']}
 
     def sync_all(self):
         """
@@ -34,18 +34,43 @@ class PluginSync(object):
 
         #sync one plugin at a time
         for p in fs_data.keys():
+            #ask whether user wants to sync this plugin
+            choice = raw_input('\nDo you want to sync the plugin ' \
+                                    + p + '? Y/n: ')
+            if choice == 'Y':
+                pass
+            elif choice == 'n':
+                print 'You have chosen not to sync ', p, '. Continuing. . .'
+                continue
+            else:
+                print 'Sorry, I didn\'nt understand your response. Skipping '\
+                        'plugin', p, '. Continuing . . .'
             #find out newest version of each plugin
             #also combines dirs and files from fs_data into single filelist
             newest = self.newest_app(p, fs_data)
-            #copy files
-            for file in newest['filelist']:
-                #copy file to central 'plugins' folder
-                self.copy_file(file, ('plugins/%s' % p))
-                #copy file from central 'plugins' folder out to other apps
-                for app in fs_data[p]['apps']:
-                    self.copy_file(file, ('applications/%' % app))
+            #Stop syncing this plugin if the user sent skip signal in
+            #self.newest_app or if that method ran into an error.
+            if newest['filelist'] != 'skip':
+                #copy files
+                for file in newest['filelist']:
+                    #skip any files excluded in self.skiplist
+                    newest_base = 'applications/' + newest['newest_app'] + '/'
+                    filename = file.replace(newest_base, '')
+                    if (p in self.skiplist.keys()) and (filename in self.skiplist[p]):
+                        print 'skipping file ', file, 'as per settings.'
+                    else:
+                        #copy file to central 'plugins' folder
+                        self.copy_file(file, ('plugins/%s' % p))
+                        #copy file from central 'plugins' folder out to other apps
+                        for app in fs_data[p]['apps']:
+                            if app != newest['newest_app']:
+                                self.copy_file(file, ('applications/%s' % app))
 
     def copy_file(self, file, newbase):
+        """
+        Copy the selected file to a new directory, preserving the ctime and
+        mtime of the original file.
+        """
         #strip 'applications/{appname}/' from file path
         relbits = file.split('/')
         newbits = relbits[2:]
@@ -53,14 +78,15 @@ class PluginSync(object):
         #strip filename from relative file path
         relpath = os.path.split(relfile)[0]
         #make new path for file in central plugins folder
-        newpath = os.path.join(newbase, newpath)
+        newpath = os.path.join(newbase, relpath)
         #create directories in plugins folder if necessary
         if not os.path.exists(newpath):
             print 'creating directory', newpath
-            os.mkdirs(newpath)
+            os.makedirs(newpath)
         # copy file to central repo
-        print 'copying ', file, 'into', newpath
-        shutil.copy2(file, os.path.join(newpath, relfile))
+        if self.verbose:
+            print 'copying ', file, 'into', newpath
+        shutil.copy2(file, os.path.join(newpath, os.path.split(relfile)[1]))
 
     def fs_data(self):
         """
@@ -107,9 +133,6 @@ class PluginSync(object):
             pls[p] = {'apps': papps,
                         'dirs': filtered_pdirs, 'files': filtered_pfiles}
 
-        pprint.pprint(pls)
-        print 'done getting folders'
-
         return pls
 
     def newest_app(self, plugin, fs_data):
@@ -141,7 +164,11 @@ class PluginSync(object):
                 newest = max(filelist.values())
                 newfile = [k for k, v in filelist.iteritems()
                                                     if v == newest][0]
-                print 'newest file in ', app, ' = ', newfile, ' ', time.strftime('%c', time.localtime(newest)), '(', newest, ' seconds)'
+                if self.verbose:
+                    print '\nnewest file in the app "', app, '" = '
+                    print newfile, ' '
+                    print 'last modified on ', time.strftime('%c',
+                            time.localtime(newest)), '(', newest, ' seconds)'
                 apptimes[app] = (newfile, newest, filelist)
             else:
                 print 'no file times to compare!'
@@ -149,41 +176,86 @@ class PluginSync(object):
         maxtime = max(a[1] for a in apptimes.values())
         maxname = [k for k, v in apptimes.iteritems() if v[1] == maxtime][0]
 
-        print maxname.capitalize(), 'seems to have the most recent version ' \
+        print '\n', maxname.capitalize(), 'seems to have the most recent version ' \
                                             'of the plugin "', plugin, '".'
         resp = raw_input('Is this correct? (Y/n)')
+        ovrrd = ''
+        maxapp = maxname
         if resp == 'Y':
             pass
-        if resp == 'n':
-            print '\navailable versions:'
-            for a in fs_data[plugin]['apps']:
-                print (fs_data[plugin]['apps'].index(a) + 1), '. ', a
-            ovrrd = raw_input('Select the most up-to-date version, or 0 to ' \
-                    'skip syncing this plugin: ')
-            maxname == fs_data[plugin]['apps'][int(ovrrd) - 1]
+        elif resp == 'n':
+            switch = 0
+            while switch < 1:
+                print '\navailable versions:'
+                for a in fs_data[plugin]['apps']:
+                    print (fs_data[plugin]['apps'].index(a) + 1), '. ', a
+                ovrrd = raw_input('Select the number for the most up-to-date '\
+                        'version, or "n" to skip syncing this plugin: ')
+                try:
+                    ovrrd = int(ovrrd)
+                    if self.verbose == True:
+                        print 'ovrrd = ', ovrrd
+                        print 'length = ', len(fs_data[plugin]['apps'])
+                    if ovrrd in range(1, len(fs_data[plugin]['apps']) + 1):
+                        maxapp = fs_data[plugin]['apps'][ovrrd - 1]
+                        switch = 1
+                    else:
+                        print 'Sorry, that didn\'t seem to be a valid option. '\
+                            'Try again'
+                        switch = 0
+                except ValueError:
+                    if ovrrd == 'n':
+                        switch = 1
+                        maxapp = None
+                    else:
+                        print 'Sorry, that didn\'t seem to be a valid option. '\
+                                'Try again'
+                        switch = 0
 
-        if maxname in fs_data[plugin]['apps']:
-            maxfiles = apptimes[maxname][2]
-            print 'Syncing plugin ', plugin, 'from app ', maxname
-            print 'Plugin files:'
-            pprint.pprint(maxfiles)
-        elif int(ovrrd) == 0:
-            maxname = None
-            maxfiles = None
+        if maxapp in fs_data[plugin]['apps']:
+            maxfiles = apptimes[maxapp][2]
+            print 'Syncing plugin ', plugin, 'from app ', maxapp
+            if self.verbose == True:
+                print 'Plugin files:'
+                pprint.pprint(maxfiles)
+        elif ovrrd == 'n':
+            maxapp = 'skip'
+            maxfiles = 'skip'
             print 'Skipping syncing plugin ', plugin
         else:
-            print 'Something went wrong!'
-            pmaxname = None
-            maxfiles = None
+            print 'Sorry, I didn\'t recognize the information you supplied.'
+            maxapp = 'skip'
+            maxfiles = 'skip'
             print 'Skipping syncing plugin ', plugin
 
-        return {'filelist': maxfiles or None, 'newest_app': maxname or None}
+        return {'filelist': maxfiles, 'newest_app': maxapp}
 
 
 def main():
-    mysync = PluginSync()
-    mysync.sync_all()
-    print "All done syncing!"
+    print '\n~~~~~~~~~~~~~~~~~~~'
+    print 'Welcome to w2ptools.\n'
+    print '\n~~~~~~~~~~~~~~~~~~~'
+    counter = 1
+    while counter < 5:
+        print 'What would you like to do right now?\n'
+        print '1. Sync installed plugins between web2py applications'
+        print '0. Exit w2ptools\n'
+        choice = raw_input('Enter a number to choose an option: ')
+        if choice == '1':
+            mysync = PluginSync()
+            mysync.sync_all()
+            print "All done syncing your plugins!"
+        elif choice == '0':
+            counter = 5
+        else:
+            if counter == 4:
+                print "\nSorry, I seem to be having trouble." \
+                        " Let's stop for now.\n"
+            else:
+                print "\nSorry, I didn't understand your choice." \
+                        " Let's try again.\n"
+            counter += 1
+    print '\nGoodbye!\n'
 
 if __name__ == '__main__':
     main()
